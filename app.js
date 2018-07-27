3f502b15621200266ca633dff12ada702e8da847
cfleur <cfleur@users.noreply.github.com>
2018-07-27 18:32:55 +0300

    Add basic layers and settings. Create methods for views with Knockout

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   "use strict";
 
+  // Set the Bing API key for Bing Maps
+  // Without your own key you will be using a limited WorldWind developer's key.
+  // See: https://www.bingmapsportal.com/ to register for your own key and then enter it below:
+  const BING_API_KEY = "";
+  if (BING_API_KEY) {
+      // Initialize WorldWind properties before creating the first WorldWindow
+      WorldWind.BingMapsKey = BING_API_KEY;
+  } else {
+      console.error("app.js: A Bing API key is required to use the Bing maps in production. Get your API key at https://www.bingmapsportal.com/");
+  }
+
+
 /**
  * The Globe encapsulates the WorldWindow object (wwd) and provides application
  * specific logic for interacting with layers.
@@ -15,13 +27,64 @@ class Globe {
     // Holds the next unique id to be assigned to a layer
     this.nextLayerId = 1;
 
-    // Add a BMNGOneImageLayer background layer. We're overriding the default 
+    // Holds a map of category and observable timestamp pairs
+    this.categoryTimestamps = new Map();
+
+    // Add a BMNGOneImageLayer background layer. We're overriding the default
     // minimum altitude of the BMNGOneImageLayer so this layer always available.
     this.addLayer(new WorldWind.BMNGOneImageLayer(), {
       category: "background",
       minActiveAltitude: 0
     });
+
   }
+
+  /**
+  * Returns an observable containing the last update timestamp for the category.
+  * @param {String} category
+  * @returns {Observable}
+  */
+  getCategoryTimestamp(category) {
+    if (!this.categoryTimestamps.has(category)) {
+      this.categoryTimestamps.set(category, ko.observable());
+    }
+    return this.categoryTimestamps.get(category);
+  }
+
+  /**
+   * Updates the timestamp for the given category.
+   * @param {String} category
+   */
+  updateCategoryTimestamp(category) {
+    let timestamp = this.getCategoryTimestamp(category);
+    timestamp(new Date());
+  }
+
+  /**
+   * Toggles the enabled state of the given layer and updates the layer
+   * catetory timestamp. Applies a rule to the 'base' layers the ensures
+   * only one base layer is enabled.
+   * @param {WorldWind.Layer} layer
+   */
+  toggleLayer(layer) {
+
+      // Multiplicity Rule: only [0..1] "base" layers can be enabled at a time
+      if (layer.category === 'base') {
+          this.wwd.layers.forEach(function (item) {
+              if (item.category === 'base' && item !== layer) {
+                  item.enabled = false;
+              }
+          });
+      }
+      // Toggle the selected layer's visibility
+      layer.enabled = !layer.enabled;
+      // Trigger a redraw so the globe shows the new layer state ASAP
+      this.wwd.redraw();
+
+      // Signal a change in the category
+      this.updateCategoryTimestamp(layer.category);
+  }
+
 
   /**
    * Adds a layer to the globe. Applies the optional options' properties to the
@@ -44,11 +107,15 @@ class Globe {
       layer.category = 'overlay'; // the default category
     }
 
-    // Assign a unique layer ID to ease layer management 
+    // Assign a unique layer ID to ease layer management
     layer.uniqueId = this.nextLayerId++;
 
     // Add the layer to the globe
     this.wwd.addLayer(layer);
+
+    // Signal that this layer category has changed
+    this.getCategoryTimestamp(layer.category);
+
   }
 
   /**
@@ -61,22 +128,118 @@ class Globe {
   }
 }
 
-let globe = new Globe("globe-canvas");
 
-// Add layers to the globe 
-  globe.addLayer(new WorldWind.BMNGLayer(), {
-    category: "base"
-  });
-  globe.addLayer(new WorldWind.CoordinatesDisplayLayer(globe.wwd), {
-    category: "setting"
-  });
-  globe.addLayer(new WorldWind.ViewControlsLayer(globe.wwd), {
-    category: "setting"
-  });
-  globe.addLayer(new WorldWind.CompassLayer(), {
-    category: "setting",
-    enabled: false
-  });
+
+
+/**
+ * View model for the layers panel.
+ * @param {Globe} globe - Our globe object
+ */
+function LayersViewModel(globe) {
+  var self = this;
+  self.baseLayers = ko.observableArray(globe.getLayers('base').reverse());
+  self.overlayLayers = ko.observableArray(globe.getLayers('overlay').reverse());
+
+  // Update the view model whenever the model changes
+  globe.getCategoryTimestamp('base').subscribe(newValue =>
+    self.loadLayers(globe.getLayers('base'), self.baseLayers));
+
+  globe.getCategoryTimestamp('overlay').subscribe(newValue =>
+    self.loadLayers(globe.getLayers('overlay'), self.overlayLayers));
+
+  // Utility to load the layers in reverse order to show last rendered on top
+  self.loadLayers = function(layers, observableArray) {
+    observableArray.removeAll();
+    layers.reverse().forEach(layer => observableArray.push(layer));
+  };
+
+  // Click event handler for the layer panel's buttons
+  self.toggleLayer = function(layer) {
+    globe.toggleLayer(layer);
+  };
+}
+
+
+/**
+ * View model for the settings.
+ * @param {Globe} globe - Our globe object (not a WorldWind.Globe)
+ */
+function SettingsViewModel(globe) {
+  var self = this;
+  self.settingLayers = ko.observableArray(globe.getLayers('setting').reverse());
+
+  // Update the view model whenever the model changes
+  globe.getCategoryTimestamp('setting').subscribe(newValue =>
+    self.loadLayers(globe.getLayers('setting'), self.settingLayers));
+
+  // Utility to load layers in reverse order
+  self.loadLayers = function(layers, observableArray) {
+    observableArray.removeAll();
+    layers.reverse().forEach(layer => observableArray.push(layer));
+  };
+
+  // Click event handler for the setting panel's buttons
+  self.toggleLayer = function(layer) {
+    globe.toggleLayer(layer);
+  };
+}
+
+
+// Create a globe
+let globe = new Globe("globe-canvas");
+// Add layers to the globe
+// Add layers ordered by drawing order: first to last
+globe.addLayer(new WorldWind.BMNGLayer(), {
+  category: "base"
+});
+globe.addLayer(new WorldWind.BMNGLandsatLayer(), {
+  category: "base",
+  enabled: false
+});
+globe.addLayer(new WorldWind.BingAerialLayer(), {
+  category: "base",
+  enabled: false
+});
+globe.addLayer(new WorldWind.BingAerialWithLabelsLayer(), {
+  category: "base",
+  enabled: false,
+  detailControl: 1.5
+});
+globe.addLayer(new WorldWind.BingRoadsLayer(), {
+  category: "overlay",
+  enabled: false,
+  detailControl: 1.5,
+  opacity: 0.75
+});
+globe.addLayer(new WorldWind.CoordinatesDisplayLayer(globe.wwd), {
+  category: "setting"
+});
+globe.addLayer(new WorldWind.ViewControlsLayer(globe.wwd), {
+  category: "setting"
+});
+globe.addLayer(new WorldWind.CompassLayer(), {
+  category: "setting",
+  enabled: false
+});
+globe.addLayer(new WorldWind.StarFieldLayer(), {
+  category: "setting",
+  enabled: false,
+  displayName: "Stars"
+});
+globe.addLayer(new WorldWind.AtmosphereLayer(), {
+  category: "setting",
+  enabled: false,
+  time: new Date()
+});
+
+
+  // Create the view models
+let layers = new LayersViewModel(globe);
+let settings = new SettingsViewModel(globe);
+
+// Bind the views to the view models
+ko.applyBindings(layers, document.getElementById('layers'));
+ko.applyBindings(settings, document.getElementById('settings'));
 
 
     // Auto-collapse the main menu when its button items are clicked
